d82d60288ec957dc56d2cde2574195c9da77ad07
Stavitskii <pochta100@mail.ru>
2022-08-13 15:00:05 +0300

    test_genre works 00:37:28

--- tests/services/test_genre.py
+++ tests/services/test_genre.py
@@ -20,25 +20,19 @@ class TestGenreService:
         genres = self.genre_service.get_all()
         assert len(genres) > 0
         assert len(genres) == 2
-#
-#     def test_create(self):
-#         genre_d = {'name': "New Name"}
-#         new_genre = self.genre_service.create(genre_d)
-#         assert new_genre.id is not None
-#
-#     def test_update(self):
-#         genre = self.genre_service.update(1)
-#
-#
-#
-#     def test_delete(self):
-#         res = self.genre_service.delete(1)
-#         assert res is None
-#
-#
-#
-#
-#
-#
-#
-#
+
+    def test_create(self):
+        genre_d = {'name': "New Name"}
+        new_genre = self.genre_service.create(genre_d)
+        assert new_genre.id is not None
+        assert new_genre.name == "Drama"
+
+    def test_update(self):
+        self.genre_service.update(1)
+
+
+
+    def test_delete(self):
+        res = self.genre_service.delete(1)
+        assert res is None
+
